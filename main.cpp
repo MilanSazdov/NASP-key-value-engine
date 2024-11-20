cea0c08d9758950903db2158b0128f6b4c2b511d
Vedran <bajic.sv10.2023@uns.ac.rs>
2024-11-21 00:57:33 +0100

    add WAL put(key, value)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <chrono>
+
 #include "wal.h"
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	cout << "hello world" << endl;
+
+int main(){
+	Wal w1;
+	
+	w1.put("dabar", "100");
+	w1.put("pcela ogromna me ujela", "ne znam kako 12345 nije deljivo sa 09434");
+	w1.put("bumbar", "200");
+
 	return 0;
 }
+
--- wal.h
+++ wal.h
@@ -1,7 +1,41 @@
 #pragma once
+#include <string>
+using namespace std;
+
+/*
+	koristi se little endian! (znaci 260 = 0x0104, ali po bajtovima je 04 01)!!!
+	ofstream outFile("ime_fajla.log", std::ios::binary);
+	outFile.write(reinterpret_cast<char*>(&objekat), sizeof(klasa_objekta));
+	ovo reinterpret_cast<char*> je tu da bi se objekat serijalizovao u binarni fajl, jer je objekat u memoriji u obliku klase, a ne kao binarni fajl, u sustini to je cast na niz charova 1/0
+	bitan je padding, sizeof(objekta) ce uvek biti 4*nesto, znaci moze biti veci, recimo 13 bytova ce biti 16...
+*/
+
+/*
+	wal je struktura koja podrzava operacije put, get i del u folder logs.
+	segment_size je velicina jednog fajla, odnosno broj recorda koje staju u njega
+	komanda za ispis binarnog fajla format-hex wal_001.log > binary_log.txt
+
+	wal.put("key", "data") upisuje data serijalizovan u fajl
+	wal.get("key") cita fajlove logova i vraca da li postoji key
+	wal.del("key") upisuje brisanje u fajl
+
+	jedan record sastoji se iz:
+
+	8 bytova crc32 hash sum
+	8 bytova timestamp (vreme u sekundama kad je upisan record)
+	1 byte tombstone (0 ako nije obrisan, 1 ako jeste)
+	8 byta keysize
+	8 byta datasize
+	keysize bytova key
+	datasize bytova data
+
+*/
 class Wal {
 	int segment_size;
 public:
 	Wal();
 	Wal(int segment_size);
+	void put(string key, string data);
+	string get(string key);
+	void del(string key);
 };
